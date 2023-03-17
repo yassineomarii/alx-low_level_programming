2fe414838a399372e7a114117d1cc3703d877a9f
yassine.omarii <its.yassineomari@gmail.com>
2023-03-17 11:05:08 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lower case.
+ *
+ * Return: 0 Always success.
+ */
+
+int main(void)
+{
+	char asciiChar = 97;
+
+	while (asciiChar <= 122)
+	{
+		putchar(asciiChar);
+		putchar('\n')
+		asciiChar++;
+	}
+}
