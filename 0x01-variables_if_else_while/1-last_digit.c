a5093197e1d042c9bedf76aad91b3043629b411e
yassine.omarii <its.yassineomari@gmail.com>
2023-03-17 10:42:21 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/** 
+ * main - Compare n values and prints the results.
+ *
+ * Return: 0 Always success.
+ **/
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
