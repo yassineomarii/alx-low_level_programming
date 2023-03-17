3e83a8732575f7616061cb2e20e044e18feda264
yassine.omarii <its.yassineomari@gmail.com>
2023-03-17 12:39:10 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lower case followed by uppercase.
+ *
+ * Return: 0 Always success.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (!(i == 101 || i == 113))
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n')
+	return (0);
+}
