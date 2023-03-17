750909e9f74c74a4f87fc2b3a0dfdb309200e987
yassine.omarii <its.yassineomari@gmail.com>
2023-03-17 13:25:18 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9.
+ *
+ * Return: 0 Always success.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
