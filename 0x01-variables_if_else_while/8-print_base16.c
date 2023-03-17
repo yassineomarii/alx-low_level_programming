2d37057a427b3d2557e942b5114c1a7f907516d6
yassine.omarii <its.yassineomari@gmail.com>
2023-03-17 13:56:37 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lower case followed by uppercase.
+ *
+ * Return: 0 Always success.
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 102; i++)
+	{
+		if (!(i <= 57 && i >= 97))
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
