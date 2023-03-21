406b9019a1252342b51e34ada67ca1e894255d29
yassine.omarii <its.yassineomari@gmail.com>
2023-03-21 14:17:26 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets 10 times.
+ */
+
+void  print_alphabet_x10(void)
+{
+	int i,j;
+
+	while (i <= 10)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
