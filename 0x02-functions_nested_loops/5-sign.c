c15672d2b084a664111076b36acd619d1f3bf099
yassine.omarii <its.yassineomari@gmail.com>
2023-03-21 15:51:51 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main"
+
+/**
+ * print_sign - Prints the sign of a number
+ *
+ * @n: Takes place as a parameter in the function described above.
+ *
+ * Retrun: 1  (n is greater than 0)
+ *	   0  (n is equal to 0)
+ *	   -1 (n is less than 0)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar("+");
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	} else {
+		_putchar("-");
+		return (-1);
+
+}
