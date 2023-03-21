89c27be91754c129e1820142f64fe43ca90b4f87
yassine.omarii <its.yassineomari@gmail.com>
2023-03-21 13:39:11 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "mian.h"
+
+/**
+ * main - Prints alphabets in lowercase using _putchar.
+ *
+ * Return: 0 Always success.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i >= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
