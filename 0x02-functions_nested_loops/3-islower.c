6081eab4db0e0a312282950d52927fcb4044a23a
yassine.omarii <its.yassineomari@gmail.com>
2023-03-21 14:37:00 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.c"
+
+/**
+ * islower - Checks if the parameter c is in lowercase.
+ *
+ * Return: 0, failure (not lowercase). 1, success (lowercase)
+ */
+
+int _islower(int c)
+{
+	if (!(c <= 97 && c >= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+
+}
