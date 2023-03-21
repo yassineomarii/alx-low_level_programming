96a9c258343dd60a4c043ea2cab408468312f28f
yassine.omarii <its.yassineomari@gmail.com>
2023-03-21 16:47:49 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Self-exlplanatory :)
+ *
+ * @x: An integer.
+ *
+ * Return: x, last digit of x value.
+ */
+
+int print_last_digit(int x)
+{
+	return (x % 10)
+}
