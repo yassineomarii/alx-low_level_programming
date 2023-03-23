4e153747d119beb0876c64bf29eb46265a47cdab
yassine.omarii <its.yassineomari@gmail.com>
2023-03-23 11:17:40 -0700

    Task 0 - Logic error.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,13 +10,10 @@
 
 int _isupper(char c)
 {
-	int i;
-
-	for (i = 65; i <= 90; i++)
+	if (c >= 65 && c <= 90)
 	{
-		if (c == i)
-			return (1);
-		else
-			return (0);
+		return (1);
+	else
+		return (0);
 	}
 }
