8c6068c3a5cab80db73c2910013637f9b0ec077a
yassine.omarii <its.yassineomari@gmail.com>
2023-03-23 13:27:49 -0700

    Task 2 - 1st try.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers.
+ *
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: value of multiplying a&b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
