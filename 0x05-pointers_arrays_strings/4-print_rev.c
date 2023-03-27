e53b713aa80276cff187b8c03c61ef4bc3d4f560
yassine.omarii <its.yassineomari@gmail.com>
2023-03-27 16:34:40 -0700

    Task 4 - 1st try.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse. 
+ *
+ * @s: A string.
+ */
+
+int print_rev(char *s)
+{
+	int lastIndex;
+
+	for (lastIndex = 0; s[lastIndex] != '\0'; lastIndex++)
+		;
+	for (lastIndex = lastIndex - 1; s[lastindex] != '\0'; lastIndex--)
+		_putchar(s[lastIndex]);
+	_putchar('\n');
+}
