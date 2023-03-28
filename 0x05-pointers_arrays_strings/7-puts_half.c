b28437ea4cd6cfe26587e972f53d0e2e7ecce2fb
yassine.omarii <its.yassineomari@gmail.com>
2023-03-27 18:15:11 -0700

    Task 7 - Syntax error fix.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	if (!((i % 2) != 0))
-		for (i = i / 2; (str[i] =! '\0'); i++)
+	if (!(i % 2 != 0))
+		for (i = i / 2; (str[i] != '\0'); i++)
 			_putchar(str[i]);
 }
