18a047d1bf11ec0860463c098bab39e1f4d40688
yassine.omarii <its.yassineomari@gmail.com>
2023-03-27 17:32:38 -0700

    Task 6 - Syntax error.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _puts2 - Prints evry other character of a string.
+ * puts2 - Prints evry other character of a string.
  *
  * @str: A string.
  */
 
-void _puts2(char *str)
+void puts2(char *str)
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
-	}
 	_putchar('\n');
 }
