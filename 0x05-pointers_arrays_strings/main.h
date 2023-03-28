c3e620435670b65a7ebcd8ac002402d9ce34d007
yassine.omarii <its.yassineomari@gmail.com>
2023-03-27 17:32:21 -0700

    main.h - Syntax error (task 6)

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 int _putchar(char x);
 int _strlen(char *s);
 void _puts(char *str);
-void _puts2(char *str);
+void puts2(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void reset_to_98(int *n);
