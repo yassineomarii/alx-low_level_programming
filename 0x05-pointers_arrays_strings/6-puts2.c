df1c2689285d65d6592366845380dc5f68ac7340
yassine.omarii <its.yassineomari@gmail.com>
2023-03-27 17:22:17 -0700

    Task 6 - 1st try...

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts2 - Prints evry other character of a string.
+ *
+ * @str: A string.
+ */
+
+void _puts2(void)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		if (a % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
