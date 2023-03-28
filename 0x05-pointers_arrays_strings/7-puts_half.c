b7e833bf8a0c05240504a879e31927929ebfb1ba
yassine.omarii <its.yassineomari@gmail.com>
2023-03-27 18:36:58 -0700

    Task 7 - Semi-colon;

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,15 @@ void puts_half(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	if (i % 2 != 0)
+	{
 		i = i - 1;
 		for (i = i / 2; (str[i] != '\0'); i++)
-			_putchar(str[i])
+			_putchar(str[i]);
+	}
 	else
+	{
 		for (i = i / 2; (str[i] != '\0'); i++)
-			_putchar(str[i])
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
