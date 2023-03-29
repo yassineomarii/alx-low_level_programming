2deaf6d54dfbe3bb4f871c1d6745f22024a45e61
yassine.omarii <its.yassineomari@gmail.com>
2023-03-29 15:53:46 -0700

    Task 0 - Syntax error 2.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,7 +16,7 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 
 	for (j = 0; src[j] != '\0'; j++)
-		dest[j] = src[j];
+		dest[i] = src[j];
 		i++
 	dest[i] = '\0';
 	return (dest);
