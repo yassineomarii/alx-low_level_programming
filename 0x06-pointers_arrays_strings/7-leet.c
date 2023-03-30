f884cd4e4f71e4b9dee93b53b124c767f5eb95ab
yassine.omarii <its.yassineomari@gmail.com>
2023-03-30 16:25:55 -0700

    Task 7 - 1st try

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * leet - Capitalizes all words of a string.
+ *
+ * @n: A string.
+ *
+ * Return: n.
+ */
+
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+		for (j = 0; j < 10; j++)
+			if (n[i] == s1[j])
+				n[i] = s2[j];
+	return (n);
+}
