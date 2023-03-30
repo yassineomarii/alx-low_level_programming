6ef81a5e91dce4cb1170f59769bf2d800893cf64
yassine.omarii <its.yassineomari@gmail.com>
2023-03-30 16:16:56 -0700

    Task 6 - Dumb solution :)

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,34 +3,38 @@
 /**
  * cap_string - Capitalizes all words of a string.
  *
- * @n: A string.
+ * @str: A string.
  *
- * Return: @s capitalized.
+ * Return: @str capitalized.
  */
 
-char *string_toupper(char *n)
+char *cap_string(char *str)
 {
-	int conversion, index, count;
+	int index = 0;
 
-	char chars[] = {' ', ',', ';', '.', '!', '?', '"',
-		'(', ')', '{', '}',  '\t', '\n', '\0'};
-	conversion = 32;
-
-	for (index = 0; entry[index] != '\0'; index++)
+	while (str[index])
 	{
-		if (entry[index] >= 'index' && entry[index] <= 'z')
-		{
-			entry[index] =  entry[index] - conversion;
-		}
-		conversion = 0;
-		for (count = 0; chars[count] != '\0'; count++)
-		{
-			if (chars[count] == entry[index])
-			{
-				conversion = 32;
-				break;
-			}
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	return (entry);
+
+	return (str);
 }
