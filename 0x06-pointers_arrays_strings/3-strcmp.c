cbfa054ffc1105bcb0703033f85ae99ae6fb9a1e
yassine.omarii <its.yassineomari@gmail.com>
2023-03-30 10:59:22 -0700

    Task 3 - While loop

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Same work as strcmp function.
+ *
+ * @s2: String.
+ * @s1: String.
+ *
+ * Return: 0, s1[i] - s2[i].
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	return (0);
+}
