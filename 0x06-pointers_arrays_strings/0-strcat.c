61f08fccaa88b7d34bface77e4cb9ad3fce489a1
yassine.omarii <its.yassineomari@gmail.com>
2023-03-30 10:26:41 -0700

    Task 0 - While loop.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,11 +13,16 @@ char *_strcat(char *dest, char *src)
 {
 	int j, i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-	for (j = 0; src[j] != '\0'; j++)
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
+	{
 		dest[i] = src[j];
-		i++
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
