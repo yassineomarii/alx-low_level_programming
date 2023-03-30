5043f7f75bf2337627dae0b1b1aba2cf0b79dff3
yassine.omarii <its.yassineomari@gmail.com>
2023-03-30 15:49:54 -0700

    Task 5 - 1st try.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Reverse an array with integers as its indexes.
+ *
+ * @n: A pointer.
+ *
+ * Return: n uppercase.
+ */
+
+char string_touper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
