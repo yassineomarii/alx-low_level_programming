b1a549d2a21ce55edbc6bbbfab2792a6a5c59bb1
yassine.omarii <its.yassineomari@gmail.com>
2023-04-03 17:00:22 -0700

    Task 0 - 1st try

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memtest - Fills memory with a contant byte.
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * @s: A pointer.
- * @b: A character.
- * @n: Number of bytes (integer).
- *
- * Return: A pointer to the memory area @s.
+ * Return: pointer to the memory area s
  */
-
-char *_memtest(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	unsigned char *mem = s, value = b;
 
 	for (i = 0; i < n; i++)
-		mem[i] = value;
-	return (mem);
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
